a111c614e644402be1c974a222b64f1aad50810d
Noeswt <sweetsummerday.n@gmail.com>
2021-03-19 20:43:42 +0900

    style終わった（scrollまだ）

--- js/script.js
+++ js/script.js
@@ -1,23 +1,68 @@
 $(function() { 
-    $(".p-header__menu__burger-btn").on("click", function() {
-        $(".p-header__menu__nav").toggleClass("menu-open");
-        $(".p-header__menu__burger-btn").toggleClass("menu-open");
-        $(".p-header__menu__burger-btn__bar").toggleClass("menu-open");
+    // menu
+    $(".js-burger-btn").on("click", function() {
+        $(".p-header__nav").toggleClass("menu-open");
+        $(".p-header__burger-btn").toggleClass("menu-open");
+        $(".p-header__burger-btn__bar").toggleClass("menu-open");
+    });
+
+    //ページ内リンクスムーススクロール
+    $('a[href^="#"]').on('click', function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop: position}, 550, "swing");
+        return false;
+    });
+
+    // メニュー押したときメニュー閉じる
+    $('a[href^="#"]').on('click', function() {
+        $('.p-header__nav').removeClass("menu-open");
+        $('.p-header__burger-btn').removeClass("menu-open");
+        $('.p-header__burger-btn__bar').removeClass("menu-open");
+
+        return false; //a要素のリンク先移動防ぐ
+    });
+
+    // 上からfade in
+    $(window).scroll(function (){
+        $('.c-fade-in').each(function(){
+            let targetElement = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 200){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
+    });
+
+    // headerの色変更
+    $(window).on("scroll", function () {
+        const sliderHeight = $(".p-fv").height();
+        if (sliderHeight - 30 < $(this).scrollTop()) {
+          $(".p-header__title__txt").addClass("js-color");
+          $(".p-header__burger-btn__bar").addClass("js-color");
+        } else {
+          $(".p-header__title__txt").removeClass("js-color");
+          $(".p-header__burger-btn__bar").removeClass("js-color");
+        }
     });
     
-    $('.p-works__slider').slick({
+    // slider
+    $('.js-slider').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
         fade: true,
-        asNavFor: '.p-works__thumb' //サムネイルのクラス名
+        asNavFor: '.js-thumb' //サムネイルのクラス名
     });
-    $('.p-works__thumb').slick({
+    $('.js-thumb').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        asNavFor: '.p-works__slider', //スライダー本体のクラス名
+        asNavFor: '.js-slider', //スライダー本体のクラス名
         focusOnSelect: true,
     });
 });
