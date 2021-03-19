3951335815d985fcac2457f3229b5346b8cfe9f4
Noeswt <sweetsummerday.n@gmail.com>
2021-03-19 14:52:24 +0900

    works スライダー実装済

--- js/script.js
+++ js/script.js
@@ -4,4 +4,20 @@ $(function() {
         $(".p-header__menu__burger-btn").toggleClass("menu-open");
         $(".p-header__menu__burger-btn__bar").toggleClass("menu-open");
     });
+    
+    $('.p-works__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.p-works__thumb' //サムネイルのクラス名
+    });
+    $('.p-works__thumb').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.p-works__slider', //スライダー本体のクラス名
+        focusOnSelect: true,
+    });
 });
